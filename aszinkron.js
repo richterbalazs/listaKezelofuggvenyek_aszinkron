527dae2b6ee31e46099881ab0c4e7108c247627d
richterbalazs <richter.balazs.krisztian@gmail.com>
2024-05-17 12:05:53 +0200

    oraimunka

---
+++ aszinkron.js
@@ -0,0 +1,37 @@
+export function getAdat(vegpont, callback){
+    fetch(vegpont, {
+        method: "GET"
+    })
+        .then((response) => response.json())
+        .then((data) => callback(data))
+        .catch((error) => console.log(error));
+}
+
+export function postAdat(vegpont,adat){
+    fetch(vegpont, {
+        method: "POST",
+        body: JSON.stringify(adat),
+    })
+        .then((response) => response.json())
+        .then((data) => callback(data))
+        .catch((error) => console.log(error));
+}
+
+export function deleteAdat(vegpont,adat, id){
+    fetch(vegpont+"/"+id, {
+        method: "DELETE",
+    })
+        .then((response) => response.json())
+        .then((data) => callback(data))
+        .catch((error) => console.log(error));
+}
+
+export function deeteAdat(vegpont,adat, id){
+    fetch(vegpont+"/"+id, {
+        method: "PUT",
+        body: JSON.stringify(adat),
+    })
+        .then((response) => response.json())
+        .then((data) => callback(data))
+        .catch((error) => console.log(error));
+}
